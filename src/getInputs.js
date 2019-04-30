deb4c9d1aab7951a6f767c434eea09e5e969ac6c
Matt Kime <matt@mattki.me>
2019-04-29 23:07:24 -0500

    fail properly, lint

---
+++ src/getInputs.js
@@ -0,0 +1,92 @@
+const { existsSync, readFileSync } = require('fs');
+const { resolve } = require('path');
+const envVarSchema = require('./envVarSchema.json');
+
+const CONFIG_FILENAME = 'github_checks_reporter.json';
+
+function getPathToRoot(currentDir = process.cwd()) {
+  const pathToPackage = `${currentDir}/${CONFIG_FILENAME}`;
+  if (existsSync(pathToPackage) || currentDir === '/') {
+    return currentDir;
+  }
+  return getPathToRoot(resolve(currentDir, '..'));
+}
+
+function getConfigFileContent() {
+  const configFilePath = `${getPathToRoot()}/${CONFIG_FILENAME}`;
+  return existsSync(configFilePath) ? JSON.parse(readFileSync(configFilePath)) : {};
+}
+
+function getAppId() {
+  return getConfigFileContent().appId;
+}
+
+function getEnvVarNameMap() {
+  const configFileContent = getConfigFileContent();
+  const configEnvVarNames = configFileContent.envVars || {};
+
+  // get default env var names from schema
+  const defaultEnvVarNames = Object.keys(envVarSchema).reduce((collector, key) => {
+    // eslint-disable-next-line no-param-reassign
+    collector[key] = envVarSchema[key].default;
+    return collector;
+  }, {});
+
+  return Object.assign({}, defaultEnvVarNames, configEnvVarNames);
+}
+
+function getEnvVars() {
+  const envVarNameMap = getEnvVarNameMap();
+
+  return Object.keys(envVarNameMap).reduce((collector, key) => {
+    // eslint-disable-next-line no-param-reassign
+    collector[key] = process.env[envVarNameMap[key]];
+    return collector;
+  }, {});
+}
+
+function getCommandLineArgs() {
+  return {
+    name: process.argv[2],
+    cmd: process.argv[3],
+    cmdArgs: process.argv.slice(4)
+  };
+}
+
+function verifyInputs(inputs) {
+  const errors = [];
+
+  Object.keys(envVarSchema).forEach(key => {
+    if (typeof inputs[key] === 'undefined') {
+      errors.push(
+        `Missing ${envVarSchema[key].doc} in environment variable '${getEnvVarNameMap[key]}'`
+      );
+    }
+  });
+
+  if (typeof inputs.appId === 'undefined') {
+    errors.push(`Missing GitHub application id in ${CONFIG_FILENAME} key 'appId'`);
+  }
+
+  if (!inputs.name || !inputs.cmd) {
+    errors.push(
+      'Please invoke with the following format: github-checks-reporter {name of task} {task command} {...task arguments}'
+    );
+  }
+
+  if (errors.length) {
+    throw new Error(errors.join('\n'));
+  }
+}
+
+module.exports = function getInputs() {
+  const inputs = {
+    appId: getAppId(),
+    ...getEnvVars(),
+    ...getCommandLineArgs()
+  };
+
+  verifyInputs(inputs);
+
+  return inputs;
+};
--- src/index.js
+++ src/index.js
@@ -8,19 +8,21 @@ const App = require('@octokit/app');
 const request = require('@octokit/request');
 const stripAnsi = require('strip-ansi');
 
-const getInputs = require('./verifyInputs');
+const getInputs = require('./getInputs');
 
- // removing 8 chars for markdown triple backtick wrap
+// removing 8 chars for markdown triple backtick wrap
 const MAX_DETAIL_BYTES = 65535 - 8;
 
-const getClientWithAuth = async function (appId, appKey, owner, repo) {
+async function getClientWithAuth(appId, appKey, owner, repo) {
   const app = new App({
     id: appId,
     privateKey: appKey
   });
 
   const jwt = app.getSignedJsonWebToken();
-  const { data: { id: installationId } } = await request('GET /repos/:owner/:repo/installation', {
+  const {
+    data: { id: installationId }
+  } = await request('GET /repos/:owner/:repo/installation', {
     owner,
     repo,
     headers: {
@@ -32,17 +34,17 @@ const getClientWithAuth = async function (appId, appKey, owner, repo) {
   const installationAccessToken = await app.getInstallationAccessToken({ installationId });
 
   return new Octokit({ auth: `token ${installationAccessToken}` });
-};
+}
 
 const prettyLogs = txt => {
   const truncatedTxt = `[truncated]\n`;
   const noAnsi = stripAnsi(txt.toString()).trim();
 
-  if(noAnsi.length === 0) {
+  if (noAnsi.length === 0) {
     return '[no output]';
   }
 
-   // Must use buffer to trim by number of bytes due to multibyte encoding
+  // Must use buffer to trim by number of bytes due to multibyte encoding
   let bufferToFit = Buffer.from(noAnsi).slice(MAX_DETAIL_BYTES * -1);
 
   const prependTxt = bufferToFit.length === MAX_DETAIL_BYTES ? truncatedTxt : '';
@@ -54,74 +56,77 @@ const prettyLogs = txt => {
 };
 
 const logRateLimit = ({
-  headers: {
-    'x-ratelimit-limit': limit,
-    'x-ratelimit-remaining': remaining
-  } }) => console.log(`GitHub checks API - ${remaining} remaining out of ${limit}/hour`);
+  headers: { 'x-ratelimit-limit': limit, 'x-ratelimit-remaining': remaining }
+  // eslint-disable-next-line no-console
+}) => console.log(`GitHub checks API - ${remaining} remaining out of ${limit}/hour`);
 
-function spawnAndSplit(cmd, cmdArgs){
+function spawnAndSplit(cmd, cmdArgs) {
   const cmdSpawnConfig = {
     stdio: ['inherit', 'pipe', 'inherit']
   };
   const child = spawn(cmd, cmdArgs, cmdSpawnConfig);
   child.stdout.pipe(process.stdout);
   return child;
-} 
+}
 
-(async function start() {
+async function start() {
   const { appKey, appId, repoSlug, commitSha, buildUrl, name, cmd, cmdArgs } = getInputs();
   const [owner, repo] = repoSlug.split('/');
   const clientWithAuth = await getClientWithAuth(appId, appKey, owner, repo);
-
-  console.log('spawn', name, cmd, cmdArgs.join(' '));
 
   const title = `${cmd} ${cmdArgs.join(' ')}`;
   const commonArgs = {
     owner,
     repo,
-    name: name,
+    name,
     head_sha: commitSha,
     details_url: buildUrl,
-    actions: [],
+    actions: []
   };
 
-  await clientWithAuth.checks.create({
+  const checkCreateResponse = await clientWithAuth.checks.create({
     ...commonArgs,
     started_at: new Date().toISOString(),
     status: 'in_progress',
     output: {
       title,
-      summary: `in progress`,
-    },
-  }).then((response) => {
-    commonArgs.check_run_id = response.data.id;
-    logRateLimit(response);
+      summary: `in progress`
+    }
   });
-
+  commonArgs.check_run_id = checkCreateResponse.data.id;
+  logRateLimit(checkCreateResponse);
 
   const childProcess = spawnAndSplit(cmd, cmdArgs);
 
   let childProcLogs = '';
+
+  // eslint-disable-next-line no-restricted-syntax
   for await (const data of childProcess.stdout) {
     childProcLogs += data;
   }
 
-  childProcess.on('close', (code) => {
-    const logs = prettyLogs(childProcLogs);
-    const errorFilePath = __dirname + '../../target/errors.json';
-    const images = [];
+  const [code] = await Promise.race([
+    new Promise(resolve => childProcess.once('close', resolve)),
+    new Promise((resolve, reject) => childProcess.once('error', reject))
+  ]);
+  const logs = prettyLogs(childProcLogs);
+  const errorFilePath = `${__dirname}../../target/errors.json`;
+  const images = [];
 
-    const annotations = fs.existsSync(errorFilePath) ? 
-      JSON.parse(fs.readFileSync(errorFilePath)) : 
-      [];
-    annotations.forEach(annotation => annotation.screenshot &&
+  const annotations = fs.existsSync(errorFilePath)
+    ? JSON.parse(fs.readFileSync(errorFilePath))
+    : [];
+  annotations.forEach(
+    annotation =>
+      annotation.screenshot &&
       images.push({
         alt: `Failure: ${annotation.title}`,
         caption: `Failure: ${annotation.title}`,
-        image_url: annotation.screenshot,
-      }));
+        image_url: annotation.screenshot
+      })
+  );
 
-    /*
+  /*
     if(fs.existsSync(errorFilePath)) {
       console.log('FILE EXISTS');
       annotations = JSON.parse(fs.readFileSync(errorFilePath));
@@ -131,26 +136,29 @@ function spawnAndSplit(cmd, cmdArgs){
           caption: `Failure: ${annotation.title}`,
           image_url: annotation.screenshot,
         }));
-        
     }else{
       console.log('DIDNT FIND FILE');
     }
     */
 
-    clientWithAuth.checks.update({
-      ...commonArgs,
-      conclusion: code === 0 ? 'success' : 'failure',
-      completed_at: new Date().toISOString(),
-      output: {
-        title,
-        summary: ` `,
-        text: `\`\`\`\n${logs}\n\`\`\``,
-        annotations,
-        images,
-      },
-    }).then((response) => {
-      logRateLimit(response);
-      process.exit(code);
-    });
+  const checkUpdateResponse = await clientWithAuth.checks.update({
+    ...commonArgs,
+    conclusion: code === 0 ? 'success' : 'failure',
+    completed_at: new Date().toISOString(),
+    output: {
+      title,
+      summary: ` `,
+      text: `\`\`\`\n${logs}\n\`\`\``,
+      annotations,
+      images
+    }
   });
-})();
+  logRateLimit(checkUpdateResponse);
+  process.exit(code);
+}
+
+start().catch(error => {
+  // eslint-disable-next-line no-console
+  console.error(error);
+  process.exit(1);
+});
